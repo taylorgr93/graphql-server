6b6c2c2d353c5e686ddacb6627f07f29329c4401
Taylor Gonzalez <taylor.gonzalez@knesysplus.com>
2024-06-13 15:24:10 -0600

    proyectop graphql

---
+++ index.js
@@ -0,0 +1,91 @@
+// index.js
+import { v1 as uuid } from "uuid";
+import { ApolloServer, gql } from "apollo-server";
+
+const persons = [
+  {
+    name: "Midu",
+    phone: "034-1234567",
+    street: "Calle Frontend",
+    city: "Barcelona",
+    id: "3d594650-3436-11e9-bc57-8b80ba54c431",
+  },
+  {
+    name: "Youssef",
+    phone: "044-1234567",
+    street: "Avenida Fullstack",
+    city: "Mataro",
+    id: "3d594650-3436-11e9-bc57-8b80ba54c431",
+  },
+  {
+    name: "Itzi",
+    street: "Pasaje Testing",
+    city: "Ibiza",
+    id: "3d594650-3436-11e9-bc57-8b80ba54c431",
+  },
+];
+
+const typeDefinitions = gql`
+  type Address {
+    street: String!
+    city: String!
+  }
+
+  type Person {
+    name: String!
+    phone: String
+    address: Address!
+    id: ID!
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons: [Person]
+    findPerson(name: String!): Person
+  }
+
+  type Mutation {
+    addPerson(
+      name: String!
+      phone: String
+      street: String!
+      city: String!
+    ): Person
+  }
+`;
+
+const resolvers = {
+  Query: {
+    personCount: () => persons.length,
+    allPersons: () => persons,
+    findPerson: (root, args) => {
+      const { name } = args;
+      return persons.find((person) => person.name === name);
+    },
+  },
+  Mutation: {
+    addPerson: (root, args) => {
+      // const { name, phone, street, city } = args;
+      const person = { ...args, id: uuid() };
+      persons.push(person);
+      return person;
+    },
+  },
+  Person: {
+    address: (root) => {
+      return {
+        street: root.street,
+        city: root.city,
+      };
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: typeDefinitions,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
